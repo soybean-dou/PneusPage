7d6009aea09c488324e03b8b30d573132d66b9b9
soybean-dou <iu98@naver.com>
2026-01-24 13:14:27 +0900

    refactor : UI refactoring

---
+++ tailwind.config.js
@@ -0,0 +1,38 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./templates/**/*.html",
+    "./static/js/**/*.js",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'brand-purple': '#663399',
+      },
+    },
+  },
+  plugins: [
+    require('daisyui'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+  ],
+  daisyui: {
+    themes: [
+      {
+        pneumo: {
+          "primary": "#b794f6",
+          "secondary": "#9f7aea",
+          "accent": "#a855f7",
+          "neutral": "#3d4451",
+          "base-100": "#ffffff",
+          "info": "#3abff8",
+          "success": "#36d399",
+          "warning": "#fbbd23",
+          "error": "#f87272",
+        },
+      },
+      "light",
+      "dark",
+    ],
+  },
+}
